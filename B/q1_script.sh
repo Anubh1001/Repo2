234a43337df5dabc12f8ae42dcab590f599a68f9
Anubh1001 <63910270+Anubh1001@users.noreply.github.com>
2020-09-18 19:19:47 +0530

    Add files via upload

---
+++ B/q1_script.sh
@@ -0,0 +1,17 @@
+cd
+cd Desktop
+cd q11
+git init
+touch 11940150.txt 
+echo "iit bhilai">11940150.txt
+cp 11940150.txt 11940370.txt
+cp 11940150.txt 11940380.txt
+mkdir Anubh Ritik Dhruv
+mv 11940150.txt Anubh 
+mv 11940370.txt Ritik
+mv 11940380.txt Dhruv
+git add Anubh
+git add Ritik
+git add Dhruv
+git commit -m "All 3 files formed and moved to 3 different folders"
+git graph 
